75c3f3030ff873544dc3a1ad62f4ac601a6a9157
Sara Javed <sarajaved@Saras-MacBook-Air.local>
2017-08-29 23:02:10 -0400

    corrected script placement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
 //winsCount starts at 0
-var winsCount = $("#winsCount");
-var winsCount = 0;
+var winsCount = 5;
+$("#winsCount").html("<h3>" + winsCount + "</h3>");
 
+/*
 //lossesCount starts at 0
 var lossesCount = $("#lossesCount");
 var lossesCount = 0;
@@ -14,13 +15,16 @@ guessLeft = 9;
 var guessList = $("#guessList");
 var guessList = [];
 
+//userGuess has to be pushed into guessList array each time
+guessList.push(userGuess);
+
 //psychicChoices is an array of all letters than psychic can choose from
 var psychicChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//psychicGuess is the choice randomly selected from psychicChoices
+//psychic randomly chooses a letter from choice array
 var psychicGuess = psychicChoices[Math.floor(Math.random() * psychicChoices.length)];
 
-//userGuess is the key the user pressed
+//user chooses letter by pressing a key
 var userGuess = event.key;
 
 //when user presses a key and choices do NOT match:
@@ -33,9 +37,12 @@ while (guessLeft > 0){
 		}
 
 }
-	
 
-	
+//when user presses a key, the game starts
+document.onkeyup = function(event) {	
+};
+
+*/
 
 	
 		
